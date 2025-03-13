06d58a40f73085e9aa3ec437669b8c31d005d6d6
NullPerson <edu1khoory@hotmail.com>
2025-03-14 01:24:24 +0400

    Rudimentary Network event code

---
+++ Assets/Hamad/Scripts/NetworkEvents.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkEvents : MonoBehaviour
+{
+
+    /*
+    public delegate void ServerConnect();
+    public ServerConnect ServerConnectEvent;
+
+    //---- Message ----
+    public delegate void ChatMessageReceived(PlayerData playerData, string message);
+    public ChatMessageReceived ChatMessageReceivedEvent;
+
+    public delegate void ChatMessageSent(PlayerData playerData, string message);
+    public ChatMessageSent ChatMessageSentEvent;
+
+    //---- color ----
+    public delegate void ColorSent(PlayerData playerData, int colorIndex);
+    public ColorSent ColorSentEvent;
+
+    public delegate void ColorReceived(PlayerData playerData, int colorIndex);
+    public ColorReceived ColorReceivedEvent;
+
+    //---- Position ----
+    public delegate void PlayerMoves(PlayerData playerData, float xPos, flost yPos, flost zPos);
+    public PlayerMoves PlayerMovesEvent;
+
+    public delegate void PlayerMoved(PlayerData playerData, float xPos, float yPos, float zPos);
+    public PlayerMoved PlayerMovedEvent;
+
+    //---- Rotation ----
+    public delegate void PlayerRotates(PlayerData playerData, float xPos, flost yPos, flost zPos);
+    public PlayerRotates PlayerRotatesEvent;
+
+    public delegate void PlayerRotated(PlayerData playerData, float xPos, float yPos, float zPos);
+    public PlayerRotated PlayerRotatedEvent;
+
+    */
+}
