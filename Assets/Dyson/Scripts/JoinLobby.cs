fed3c3d9209b44a9058141dd429dc14536f4fe79
Dyson <dysonkui@gmail.com>
2025-04-10 00:48:22 +0400

    More lobby logic  + added bool to ClientState

--- Assets/Dyson/Scripts/JoinLobby.cs
+++ Assets/Dyson/Scripts/JoinLobby.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using Leonardo.Scripts.ClientRelated;
 using UnityEngine;
 
@@ -12,10 +13,17 @@ namespace Dyson.GPG222.Lobby
         public GameObject lobbyCanvas;
         public List<ClientState> players;
         public ClientState playersId;
-
+        public Lobby _lobby;
+        public TcpClient client;
+        
         private void Start()
         {
             playersId = new ClientState();
+
+            if (_lobby == null)
+            {
+                _lobby = FindObjectOfType<Lobby>();
+            }
         }
 
         public void JoinLobbyButton()
@@ -23,6 +31,15 @@ namespace Dyson.GPG222.Lobby
             joinLobbyCanvas.SetActive(false);
             lobbyCanvas.SetActive(true);
             Debug.Log("Who joined my lobby?: " + playersId.ClientId);
+
+            if (_lobby != null)
+            {
+                _lobby.AddPlayerToLobby(playersId, client, playersId.ClientId);
+                Debug.Log(playersId);
+                Debug.Log(playersId.ClientId);
+                Debug.Log("Player added to lobby");
+            }
         }
+        
     }
 }
