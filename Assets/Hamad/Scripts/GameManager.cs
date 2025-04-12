2c9655dcf80c737f3c1d46e7fb90faa0dfe1308d
NullPerson <edu1khoory@hotmail.com>
2025-04-12 15:28:55 +0400

    Setting up the gamemanager and restart packets

---
+++ Assets/Hamad/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    //game waits for the players to join and start the game
+
+    //once the game starts, two lists will be there, one for the players, and one for the players who lost
+
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private GameObject[] players;
+    [SerializeField] private GameObject[] lostPlayers;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
